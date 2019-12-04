7705d17146f5888a005c17d832660ee4f684cea2
Iqra Masroor <masrooriqra@gmail.com>
2019-12-04 05:08:07 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,20 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  element = array.pop
+  p element
+end  
+
+def pop_with_args(array)
+  removed_items = array.pop(2)
+  p removed_items
+end  
+
+def using_shift(array)
+  array.shift
